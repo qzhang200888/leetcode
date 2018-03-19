1e6326814e641137e23e4c3c9ffa3ed69c2715b5
qzhang200888 <qzhang200888@gmail.com>
2018-03-19 11:51:48 -0700

    add 49

---
+++ java/49GroupAnagrams.java
@@ -0,0 +1,17 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<String, List<String>>();
+        for (int i = 0; i < strs.length; ++i) {
+            char[] chars = strs[i].toCharArray();
+            Arrays.sort(chars);
+            String key = String.valueOf(chars);
+            if (!map.containsKey(key)) {
+                map.put(key, new ArrayList<String>());
+            }
+            
+            map.get(key).add(strs[i]);
+        }
+        
+        return new ArrayList<List<String>>(map.values());
+    }
+}
