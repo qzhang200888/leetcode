51e85261ebb7e9f431e767cb5d8b5475e1527048
qzhang200888 <qzhang200888@gmail.com>
2018-05-10 10:58:41 -0700

    add 76

---
+++ java/76MinimumWindowSubstring.java
@@ -0,0 +1,84 @@
+class Solution {
+    public String minWindow(String s, String t) {
+        if (s == null || s.length() == 0 ||
+            t == null || t.length() == 0)
+            return "";
+        Map<Character, Integer> map =
+            new HashMap<Character, Integer>();
+        
+        int count = 0;
+        for (char c: t.toCharArray()) {
+            int fre = map.getOrDefault(c, 0);
+            if (fre == 0)
+                ++count;
+            map.put(c, fre + 1);
+        }
+        
+        int start = 0, end = 0;
+        int len = Integer.MAX_VALUE, minStart = -1, minEnd = -1;
+        char[] charList = s.toCharArray();
+        while (end < charList.length) {
+            if (count > 0) {
+                if (map.containsKey(charList[end])) {
+                    int fre = map.get(charList[end]);
+                    --fre;
+                    if (fre == 0)
+                        --count;
+                    map.put(charList[end], fre);
+                }
+                
+                ++end;
+            } else {
+                while (count == 0 && start < end) {
+                    if (map.containsKey(charList[start])) {
+                        int fre = map.get(charList[start]);
+                
+                        if (fre == 0)
+                            ++count;
+                        map.put(charList[start], ++fre);
+                    }
+                    ++start;   
+                }
+                
+                if (count > 0)
+                    --start;
+
+                int newLen = end - start + 1;
+                if (newLen < len) {
+                    len = newLen;
+                    minStart = start;
+                    minEnd = end;
+                }
+                
+                ++start;
+            }
+        }
+        
+        if (count == 0) {
+            while (count == 0 && start < end) {
+                if (map.containsKey(charList[start])) {
+                    int fre = map.get(charList[start]);
+                
+                    if (fre == 0)
+                        ++count;
+                    map.put(charList[start], ++fre);
+                }
+                ++start;   
+            }
+                
+            if (count > 0)
+                --start;
+            int newLen = end - start + 1;
+            if (newLen < len) {
+                len = newLen;
+                minStart = start;
+                minEnd = end;
+            }
+        }
+        
+        if (len == Integer.MAX_VALUE)
+            return "";
+        
+        return s.substring(minStart, minEnd);
+    }
+}
