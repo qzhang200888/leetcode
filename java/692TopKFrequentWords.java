02ca68b19e8faaefab768f96e1654de01a3c9c1b
Joanne Xiao <chuan_xiao@apple.com>
2018-06-03 12:24:45 -0700

    add 692

---
+++ java/692TopKFrequentWords.java
@@ -0,0 +1,35 @@
+class Solution {
+    public List<String> topKFrequent(String[] words, int k) {
+        List<String> res = new ArrayList<String>();
+        if (words == null)
+            return res;
+        
+        Map<String, Integer> map = new HashMap<>();
+        for (int i = 0; i < words.length; ++i) {
+            int fre = map.getOrDefault(words[i], 0);
+            map.put(words[i], fre + 1);
+        }
+        
+        PriorityQueue<String> pq = new PriorityQueue<String>(10, new Comparator<String>() {
+            @Override
+            public int compare(String s1, String s2) {
+                if (map.get(s2) == map.get(s1))
+                    return s1.compareTo(s2);
+                return map.get(s2) - map.get(s1);
+            }
+        });
+        
+        Set<String> set = new HashSet<String>();
+        for (int i = 0; i < words.length; ++i) {
+            if (!set.contains(words[i])) {
+                pq.add(words[i]);
+                set.add(words[i]);
+            }
+        }
+        
+        for (int i = 0; i < k; ++i)
+            res.add(pq.poll());
+        
+        return res;
+    }
+}
