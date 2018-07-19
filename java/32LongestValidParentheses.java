e2ccc0aa60dbed2445ab3f3c3c847663ee247ed1
Qing Zhang <qing.zhang@byton.com>
2018-07-18 23:34:54 -0700

    add 32

---
+++ java/32LongestValidParentheses.java
@@ -0,0 +1,47 @@
+class Solution {
+    public int longestValidParentheses(String s) {
+        if (s == null)
+            return 0;
+            
+        int max = 0;
+        int leftCount = 0;
+        int rightCount = 0;
+        for (int i = 0; i < s.length(); ++i) {
+            char c = s.charAt(i);
+            if (c == ')') {
+                ++rightCount;
+                if (rightCount > leftCount) {
+                    leftCount = 0;
+                    rightCount = 0;
+                } else if (rightCount == leftCount) {
+                    max = Math.max(max, 2 * rightCount);
+                }
+            } else {
+                ++leftCount;
+            }
+            
+
+        }
+ 
+        leftCount = 0;
+        rightCount = 0;
+        for (int i = s.length() - 1; i >= 0; --i) {
+            char c = s.charAt(i);
+            if (c == '(') {
+                ++leftCount;
+                if (leftCount > rightCount) {
+                    leftCount = 0;
+                    rightCount = 0;
+                } else if (rightCount == leftCount)  {
+                    max = Math.max(max, 2 * leftCount);
+                }
+            } else {
+                ++rightCount;
+            }
+            
+
+        }
+        
+        return max;
+    }
+}
