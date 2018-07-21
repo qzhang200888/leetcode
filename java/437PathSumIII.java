657c2f0f5c1220cd494d187be4a0f6c10fed4002
Qing Zhang <qing.zhang@byton.com>
2018-07-21 12:36:38 -0700

    add 437

--- java/437PathSumIII.java
+++ java/437PathSumIII.java
@@ -35,3 +35,41 @@ class Solution {
         return res;
     }
 }
+
+// use presum
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    int pathSum(TreeNode n, int presum, int sum, Map<Integer, Integer> map) {
+        int count = 0;
+        if (n == null)
+            return count;
+        
+        presum += n.val;
+        int key = presum - sum;
+        if (map.containsKey(key))
+            count += map.get(key);
+        map.put(presum, map.getOrDefault(presum, 0) + 1);
+        
+        count += pathSum(n.left, presum, sum, map);
+        count += pathSum(n.right, presum, sum, map);
+        
+        map.put(presum, map.get(presum) - 1);
+        
+        return count;
+    }
+    
+    public int pathSum(TreeNode root, int sum) {
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+        return pathSum(root, 0, sum, map);
+    }
+}
