4348d8d2c3fe9606095d229e1a0aedd79d2771bf
Qing Zhang <qing.zhang@byton.com>
2018-08-15 19:31:10 -0700

    add 88

--- java/88MergeSortedArray.java
+++ java/88MergeSortedArray.java
@@ -25,3 +25,25 @@ class Solution {
         }
     }
 }
+///
+
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        if (nums2 == null || nums2.length == 0)
+            return;
+        
+        int pos = m + n - 1;
+        int first = m - 1, second = n - 1;
+        while (first >= 0 && second >= 0) {
+            if (nums1[first] >= nums2[second]) {
+                nums1[pos--] = nums1[first--];
+            } else {
+                nums1[pos--] = nums2[second--];
+            }
+        }
+        
+        while (second >= 0) {
+            nums1[pos--] = nums2[second--];
+        }
+    }
+}
