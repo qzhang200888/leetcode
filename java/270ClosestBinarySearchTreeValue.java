84170045ea6784e50c9fc769e8dfac4f13c93adc
Qing Zhang <qing.zhang@byton.com>
2018-09-16 18:16:54 -0700

    update 270

--- java/270ClosestBinarySearchTreeValue.java
+++ java/270ClosestBinarySearchTreeValue.java
@@ -36,3 +36,54 @@ public class Solution {
         return root.val;
     }
 }
+
+/////
+
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */
+
+public class Solution {
+    long ceiling = Long.MAX_VALUE;
+    long floor = Long.MIN_VALUE;
+    /**
+     * @param root: the given BST
+     * @param target: the given target
+     * @return: the value in the BST that is closest to the target
+     */
+    public int closestValue(TreeNode root, double target) {
+        if (((double)root.val) > target) {
+            ceiling = Math.min(root.val, ceiling);
+            if (root.left == null) {
+                if (ceiling - target < target - floor) {
+                    return (int)ceiling;
+                }
+                return (int)floor;
+            } 
+            
+            return closestValue(root.left, target);
+        }
+        
+        if (((double)root.val) < target) {
+            floor = Math.max(root.val, floor);
+            if (root.right == null) {
+                if (ceiling - target < target - floor) {
+                    return (int)ceiling;
+                }
+                return (int)floor;
+            }
+            
+            return closestValue(root.right, target);
+        }
+        
+        return root.val;
+    }
+}
