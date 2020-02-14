2a26b4af3fe14a9456ce6eb9da01d07ff462b863
Qing Zhang <qzhang200888@gmail.com>
2020-02-14 10:16:57 -0800

    update 25

--- java/25ReverseNodesink-Group.java
+++ java/25ReverseNodesink-Group.java
@@ -44,3 +44,57 @@ class Solution {
         return head;
     }
 }
+
+==============================================================
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    ListNode revert(ListNode head) {
+        if (head == null || head.next == null)
+            return head;
+        
+        ListNode cur = head.next;
+        head.next = null;
+        ListNode prev = head;
+        
+        while (cur != null) {
+            ListNode next = cur.next;
+            cur.next = prev;
+            prev = cur;
+            cur = next;
+        }
+        
+        return prev;
+    }
+
+    public ListNode reverseKGroup(ListNode head, int k) {
+        if (head == null)
+            return head;
+        int count = 0;
+        ListNode node = head;
+        while (node != null) {
+            ++count;
+            if (count >= k)
+                break;
+            node = node.next;
+        }
+        
+        if (count < k)
+            return head;
+        
+        ListNode next = node.next;
+        node.next = null;
+        
+        ListNode res = revert(head);
+        head.next = reverseKGroup(next, k);
+        
+        return res;
+    }
+}
