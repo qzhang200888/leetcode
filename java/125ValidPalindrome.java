e7f60844dca62b1b48ea7b73e0f4982e7a6b2890
Qing Zhang <qzhang200888@gmail.com>
2020-04-03 10:55:34 -0700

    update 125

--- java/125ValidPalindrome.java
+++ java/125ValidPalindrome.java
@@ -21,3 +21,44 @@ class Solution {
         return true;
     }
 }
+
+==================================
+
+class Solution {
+    
+    boolean isValid(char c) {
+        return (c >= '0' && c <= '9') ||
+            (c >= 'a' && c <= 'z') ||
+            (c >= 'A' && c <= 'Z') ;
+    }
+    
+    boolean compare(char c1, char c2) {
+        if (c1 == c2)
+            return true;
+        if ( (c1 >= 'a' && c1 <= 'z')  && (c1 - 'a') == (c2 - 'A'))
+            return true;
+        if ((c1 >= 'A' && c1 <= 'Z')  && (c1 - 'A') == (c2 - 'a'))
+            return true;
+        
+        return false;
+    }
+    
+    public boolean isPalindrome(String s) {
+        int i = 0, j = s.length() - 1;
+        
+        while (i < j) {
+            if (!isValid(s.charAt(i)))
+                ++i;
+            else if (!isValid(s.charAt(j)))
+                --j;
+            else {
+                if (!compare(s.charAt(i), s.charAt(j)))
+                    return false;
+                ++i;
+                --j;
+            }
+        }
+        
+        return true;
+    }
+}
