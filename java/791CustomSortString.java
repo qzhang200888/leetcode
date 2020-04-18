ebd78d89e319f7e3d5ffdae2561af2297f9e1865
Qing Zhang <qzhang200888@gmail.com>
2020-04-18 13:40:45 -0700

    update 791

--- java/791CustomSortString.java
+++ java/791CustomSortString.java
@@ -1,3 +1,43 @@
+
+class Solution {
+    public String customSortString(String S, String T) {
+        Map<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < S.length(); ++i) {
+            map.put(S.charAt(i), i);
+        }
+
+        Character[] arrs = new Character[T.length()];
+        for (int i = 0; i < T.length(); ++i) {
+            arrs[i] = T.charAt(i);
+        }
+        
+        Arrays.sort(arrs, new Comparator<Character>() {
+            @Override
+            public int compare(Character c1, Character c2) {
+                if (!map.containsKey(c1) && !map.containsKey(c2))
+                    return 0;
+            
+                if (!map.containsKey(c1))
+                    return 1;
+                
+                if (!map.containsKey(c2))
+                    return -1;
+                
+                return map.get(c1) - map.get(c2);
+            }
+        });
+        
+        StringBuilder sb = new StringBuilder();
+        for (char c: arrs) {
+            sb.append(c);
+        }
+        
+        return sb.toString();
+    }
+}
+
+====================================================================================
+
 class Solution {
     public String customSortString(String S, String T) {
         char[] chars = T.toCharArray();
