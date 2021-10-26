bbe7b45cac3604e1a6f064fbaf48e3595970df7b
Nz1228 <nancyzhang2017@gmail.com>
2021-10-25 20:08:52 -0700

    add 390

---
+++ java/390EliminationGame.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int lastRemaining(int n) {
+        int last = n;
+        boolean forward = true;
+        int diff = 1;
+        while (n > 1) {
+            if (!forward || (forward && n % 2 != 0)) {
+                last -= diff;
+            }
+            n /= 2;
+            diff *= 2;
+            forward = !forward;
+        }
+        
+        return last;
+    }
+}
