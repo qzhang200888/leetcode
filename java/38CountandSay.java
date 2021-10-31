bd1c9cb8a3e4e7d97643508c8b5736ee25866b21
Nz1228 <nancyzhang2017@gmail.com>
2021-10-31 08:29:27 -0700

    add 38 & 72

--- java/38CountandSay.java
+++ java/38CountandSay.java
@@ -1,3 +1,43 @@
+
+class Solution {
+    public String countAndSay(int n) {
+        if (n < 1)
+            return "";
+        if (n == 1)
+            return "1";
+        String s = countAndSay(n - 1);
+        
+        StringBuilder sb = new StringBuilder();
+        int count = 0;
+        char c = '\0';
+        for (int i = 0; i < s.length(); ++i) {
+            char ch = s.charAt(i);
+            if (i == 0) {
+                count = 1;
+                c = ch;
+                continue;
+            }
+            
+            if (c != ch) {
+                sb.append(count);
+                sb.append(c);
+                c = ch;
+                count = 1;
+                continue;
+            }
+            
+            ++count;
+        }
+        
+        sb.append(count);
+        sb.append(c);
+        
+        return sb.toString();
+    }
+}
+
+==================================================
+
 class Solution {  
     public String countAndSay(int n) {
         String s = "1";
--- java/72EditDistance.java
+++ java/72EditDistance.java
@@ -1,3 +1,31 @@
+
+class Solution {
+    public int minDistance(String word1, String word2) {
+        int[][] dist = new int[word1.length() + 1][word2.length() + 1];
+        for (int i = 1; i <= word2.length(); ++i)
+            dist[0][i] = i;
+        for (int i = 1; i <= word1.length(); ++i)
+            dist[i][0] = i;
+        
+        for (int i = 1; i <= word1.length(); ++i) {
+            for (int j = 1; j <= word2.length(); ++j) {
+                int op = dist[i-1][j] + 1;
+                op = Math.min(op, dist[i][j - 1] + 1);
+                if (word1.charAt(i-1) == word2.charAt(j-1)) {
+                    op = Math.min(op, dist[i - 1][j - 1]);
+                } else {
+                    op = Math.min(op, dist[i - 1][j - 1] + 1);
+                }
+                
+                dist[i][j] = op;
+            }
+        }
+        
+        return dist[word1.length()][word2.length()];
+    }
+}
+
+======================================
 class Solution {
     public int minDistance(String word1, String word2) {
         if (word1 == null && word2 == null)
