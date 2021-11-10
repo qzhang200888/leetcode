bd431a4d09f2cb5157f52446d910229c37d828fa
Nz1228 <nancyzhang2017@gmail.com>
2021-11-10 13:11:06 -0800

    update 131

--- java/131PalindromePartitioning.java
+++ java/131PalindromePartitioning.java
@@ -7,19 +7,20 @@ class Solution {
         }
         return true;
     }
+    
     void partition(String s, int start, List<String> lst, List<List<String>> res) {
         if (start >= s.length()) {
             res.add(new ArrayList<>(lst));
             return;
         }
-        
-        for (int i = start + 1; i <= s.length(); ++i) {
-            if (isPalindrome(s, start, i - 1)) {
+
+        for (int i = start + 1; i <= s.length(); ++i) {            
+            if (start == i - 1 || isPalindrome(s, start, i - 1)) {
                 lst.add(s.substring(start, i));
                 partition(s, i, lst, res);
                 lst.remove(lst.size() - 1);
             }
-        }
+        }   
     }
 
     public List<List<String>> partition(String s) {
