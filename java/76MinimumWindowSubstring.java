620d80251e301c4ce012e29593bf64a6b787b1de
Nz1228 <nancyzhang2017@gmail.com>
2021-11-23 12:58:50 -0800

    update 76

--- java/76MinimumWindowSubstring.java
+++ java/76MinimumWindowSubstring.java
@@ -1,3 +1,74 @@
+
+class Solution {
+    int getIdx(char c) {
+        if (c >= 'a' && c <= 'z')
+            return c - 'a';
+        return 26 + c - 'A';
+    }
+
+    public String minWindow(String s, String t) {
+        int[] map = new int[52];
+        Arrays.fill(map, Integer.MIN_VALUE);
+        int count = 0;
+        for (int i = 0; i < t.length(); ++i) {
+            char c = t.charAt(i);
+            int idx = getIdx(c);
+            if (map[idx] == Integer.MIN_VALUE) {
+                ++count;
+                map[idx] = 0;
+            }
+            ++map[idx];
+        }
+        
+        int start = 0, end = 0;
+        int min = Integer.MAX_VALUE, minStart = -1, minEnd = -1;
+        
+        char c;
+        int idx;
+        for (end = start; end < s.length(); ++end) {
+            c = s.charAt(end);
+            idx = getIdx(c);
+            if (map[idx] == Integer.MIN_VALUE)
+                continue;
+
+            --map[idx];
+            if (map[idx] == 0)
+                --count;
+            
+            if (count == 0) {
+                while (start <= end && count == 0) {
+                    c = s.charAt(start);
+                    idx = getIdx(c);
+                    if (map[idx] == Integer.MIN_VALUE) {
+                        ++start;
+                        continue;
+                    }
+                    
+                    if (min > end - start) {
+                        min = end - start;
+                        minStart = start;
+                        minEnd = end;
+                    }
+                    
+                    ++map[idx];
+                    if (map[idx] == 1) {
+                        ++count;
+                    }
+                    
+                    ++start;
+                }
+            }
+        }
+
+        if (min == Integer.MAX_VALUE)
+            return "";
+    
+        return s.substring(minStart, minEnd + 1);
+    }
+}
+
+====================================================================
+
 class Solution {
     public String minWindow(String s, String t) {
         if (s == null || s.length() == 0 ||
