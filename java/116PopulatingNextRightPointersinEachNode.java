437a0971ee8fc72efe2154a7cc45d15b0b166463
Nz1228 <nancyzhang2017@gmail.com>
2021-11-24 08:06:38 -0800

    update 116

--- java/116PopulatingNextRightPointersinEachNode.java
+++ java/116PopulatingNextRightPointersinEachNode.java
@@ -1,3 +1,54 @@
+
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {}
+    
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+};
+*/
+
+class Solution {    
+    public Node connect(Node root) {
+        if (root == null)
+            return null;
+
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            Node prev = null;
+            int size = queue.size();
+            for (int i = 0; i < size; ++i) {
+                Node n = queue.poll();
+                if (n.left != null)
+                    queue.add(n.left);
+                if (n.left != null)
+                    queue.add(n.right);
+                if (prev != null)
+                    prev.next = n;
+                prev = n;
+            }
+        }
+        return root;
+    }
+}
+
+
+======================================================
 /**
  * Definition for binary tree with next pointer.
  * public class TreeLinkNode {
