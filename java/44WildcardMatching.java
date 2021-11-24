3d0afc7dbdbd2b53dd9f3d8e6b0399b8de00a367
Nz1228 <nancyzhang2017@gmail.com>
2021-11-23 18:03:32 -0800

    update 44

--- java/44WildcardMatching.java
+++ java/44WildcardMatching.java
@@ -1,3 +1,40 @@
+
+class Solution {
+    public boolean isMatch(String s, String p) {        
+        boolean[][] res = new boolean[s.length() + 1][p.length() + 1];
+        
+        for (int i = res.length - 1; i >= 0; --i) {
+            for (int j = res[0].length - 1; j >= 0; --j) {
+                if (i == res.length - 1 && j == res[0].length - 1) {
+                    res[i][j] = true;
+                } else if (j == res[0].length - 1) {
+                    res[i][j] = false;
+                } else if (i == res.length - 1) {
+                    char c = p.charAt(j);
+                    if (c != '*')
+                        res[i][j] = false;
+                    else res[i][j] = res[i][j + 1];
+                } else {
+                    char c1 = s.charAt(i);
+                    char c2 = p.charAt(j);
+                
+                    if (c1 == c2 || c2 == '?') {
+                        res[i][j] = res[i+1][j+1];
+                    } else if (c2 != '*') {
+                        res[i][j] = false;
+                    } else {
+                        res[i][j] = res[i + 1][j] || res[i][j + 1];
+                    }
+                }
+            }
+        }
+
+        return res[0][0];
+    }
+}
+
+===============================================================================
+
 class Solution {
     public boolean isMatch(String s, String p) {        
         boolean[][] res = new boolean[s.length() + 1][p.length() + 1];
