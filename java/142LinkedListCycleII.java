ff60ebfa1b720badbce5d9157cfc6671e92b1474
Nz1228 <nancyzhang2017@gmail.com>
2021-11-25 12:40:41 -0800

    add 142

---
+++ java/142LinkedListCycleII.java
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        Set<ListNode> set = new HashSet<>();
+        ListNode node = head;
+        while (node != null) {
+            if (set.contains(node))
+                return node;
+            set.add(node);
+            node = node.next;
+        }
+        
+        return null;
+    }
+}
