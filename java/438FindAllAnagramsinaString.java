90dda760e5676dce4dfa9b640a040d339cd01131
Nz1228 <nancyzhang2017@gmail.com>
2021-11-25 09:07:20 -0800

    update 438

--- java/438FindAllAnagramsinaString.java
+++ java/438FindAllAnagramsinaString.java
@@ -1,5 +1,51 @@
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
+        if (s == null || p == null || s.length() == 0 || s.length() < p.length())
+            return new ArrayList<>();
+
+        int[] letters = new int[26];
+        char[] patterns = p.toCharArray();
+        char[] strs = s.toCharArray();
+        
+        for (char c: patterns) {
+            ++letters[c - 'a'];
+        }
+        
+        int len = patterns.length;
+        int count = len;
+        int i = 0;
+        for (; i < len; ++i) {
+            --letters[strs[i] - 'a'];
+            if (letters[strs[i] - 'a'] >= 0)
+                --count;
+        }
+        
+        List<Integer> lst = new ArrayList<>();
+        if (count == 0)
+            lst.add(0);
+
+        for (; i < strs.length; ++i) {
+            ++letters[strs[i - len] - 'a'];
+            if (letters[strs[i - len] - 'a'] > 0)
+                ++count;
+
+            --letters[strs[i] - 'a'];
+            if (letters[strs[i] - 'a'] >= 0)
+                --count;
+
+            if (count == 0) {
+                lst.add(i - len + 1);
+            }
+        }
+        
+        return lst;
+    }
+}
+
+==============================================
+
+class Solution {
+    public List<Integer> findAnagrams(String s, String p) {
         List<Integer> lst = new ArrayList<Integer>();
         if (s == null || p == null || s.length() < p.length())
             return lst;
