ec5daf43bbf0b663f75cbbfb4159250df4b311ef
Nz1228 <nancyzhang2017@gmail.com>
2021-11-25 08:14:25 -0800

    update 140

--- java/140WordBreakII.java
+++ java/140WordBreakII.java
@@ -1,3 +1,42 @@
+
+class Solution {
+    public List<String> wordBreak(String s, List<String> wordDict) {
+        Set<String> set = new HashSet<>();
+        for (String str: wordDict) {
+            set.add(str);
+        }
+        
+        Map<Integer, List<String>> map = new HashMap<>();
+        for (int i = 0; i < s.length(); ++i) {
+            List<String> lst = new ArrayList<>();
+            String substr = s.substring(0, i + 1);
+            if (set.contains(substr)) {
+                lst.add(substr);
+            }
+            for (int j = i - 1; j >= 0; --j) {
+                if (!map.containsKey(j))
+                    continue;
+                substr = s.substring(j + 1, i + 1);
+                if (!set.contains(substr))
+                    continue;
+                List<String> preList = map.get(j);
+                for (String lststr: preList) {
+                    lst.add(lststr + " " + substr);
+                }
+            }
+            
+            if (lst.size() > 0)
+                map.put(i, lst);
+        }
+        
+        if (map.containsKey(s.length() - 1))
+            return map.get(s.length() - 1);
+
+        return new ArrayList<String>();
+    }
+}
+
+==================================================
 class Solution {
     List<String> wordBreak(String s, Set<String> dict) {
         boolean[] b = new boolean[s.length()];
