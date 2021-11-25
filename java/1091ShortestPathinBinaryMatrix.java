558d113bdccdd617f1e67665bd6fdd5894734d3b
Nz1228 <nancyzhang2017@gmail.com>
2021-11-25 14:03:23 -0800

    update 1091

--- java/1091ShortestPathinBinaryMatrix.java
+++ java/1091ShortestPathinBinaryMatrix.java
@@ -1,3 +1,75 @@
+
+
+class Solution {
+    public int shortestPathBinaryMatrix(int[][] grid) {
+        if (grid[0][0] != 0 || grid[grid.length - 1][grid[0].length - 1] != 0)
+            return -1;
+        
+        Queue<int[]> queue = new LinkedList<>();
+        queue.offer(new int[]{0, 0});
+        grid[0][0] = 1;
+    
+        int len = 1;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; ++i) {
+                int[] elem = queue.poll();
+                int row = elem[0];
+                int col = elem[1];
+                if (row == grid.length - 1 && col == grid[0].length - 1)
+                    return len;
+            
+                if (row > 0 && grid[row-1][col] == 0) {
+                    queue.offer(new int[]{row-1, col});
+                    grid[row-1][col] = 1;
+                }
+                
+                if (col > 0 && grid[row][col-1] == 0) {
+                    queue.offer(new int[]{row, col-1});
+                    grid[row][col-1] = 1;
+                }
+                
+                if (col > 0 && row > 0 && grid[row-1][col-1] == 0) {
+                    queue.offer(new int[]{row-1, col-1});
+                    grid[row-1][col-1] = 1;
+                }
+                
+                if (row < grid.length - 1 && grid[row+1][col] == 0) {
+                    queue.offer(new int[]{row+1, col});
+                    grid[row+1][col] = 1;
+                }
+                
+                if (row < grid.length - 1 && col > 0 && grid[row+1][col-1] == 0) {
+                    queue.offer(new int[]{row+1, col-1});
+                    grid[row+1][col-1] = 1;
+                }
+                
+                if (col < grid[0].length - 1 && grid[row][col+1] == 0) {
+                    queue.offer(new int[]{row, col+1});
+                    grid[row][col+1] = 1;
+                }
+                
+                if (col < grid[0].length - 1 && row > 0 && grid[row-1][col+1] == 0) {
+                    queue.offer(new int[]{row-1, col+1});
+                    grid[row-1][col+1] = 1;
+                }
+                
+                if (col < grid[0].length - 1 && row < grid.length - 1  && grid[row+1][col+1] == 0) {
+                    queue.offer(new int[]{row+1, col+1});
+                    grid[row+1][col+1] = 1;
+                }
+            }
+            
+            ++len;
+        }
+        
+        return -1;
+    }
+}
+
+
+===============================================================================
+
 class Solution {
     public int shortestPathBinaryMatrix(int[][] grid) {
         if (grid[0][0] != 0 || grid[grid.length - 1][grid[0].length - 1] != 0)
