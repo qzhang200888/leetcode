110b972b8d9dcf57fd90e90ec334deca897bfd31
Nz1228 <nancyzhang2017@gmail.com>
2021-11-26 17:50:27 -0800

    update 827

--- java/827MakingALargeIsland.java
+++ java/827MakingALargeIsland.java
@@ -19,6 +19,89 @@ class Solution {
         if (max == grid.length * grid[0].length)
             return max;
         
+        ++max;
+        for (int i = 0; i < grid.length; ++i) {
+            for (int j = 0; j < grid[0].length; ++j) {
+                if (grid[i][j] != 0)
+                    continue;
+                int ar = 1;
+                List<Integer> nlst = new ArrayList<>();
+                if (i > 0 && grid[i-1][j] != 0) {
+                    addUnique(nlst, grid[i-1][j]);
+                }
+                if (j > 0 && grid[i][j-1] != 0) {
+                    addUnique(nlst, grid[i][j-1]);
+                }
+                if (i < grid.length - 1 && grid[i+1][j] != 0) {
+                    addUnique(nlst, grid[i+1][j]);
+                }
+                if (j< grid[0].length - 1 && grid[i][j+1] != 0) {
+                    addUnique(nlst, grid[i][j + 1]);
+                }
+                
+                if (nlst.size() > 1) {
+                    for(int g: nlst) {
+                        ar += lst.get(g - 2);
+                    }
+                    if (max < ar) {
+                        max = ar;
+                    }
+                }
+            }
+        }
+        
+        return max;
+    }
+    
+    int area(int[][] grid, int r, int c, int group) {
+        if (r < 0 || c < 0 || r >= grid.length || c >= grid[0].length)
+            return 0;
+
+        if (grid[r][c] != 1)
+            return 0;
+
+        grid[r][c] = group;
+        int a = 1;
+        a += area(grid, r - 1, c, group);
+        a += area(grid, r + 1, c, group);
+        a += area(grid, r, c - 1, group);
+        a += area(grid, r, c + 1, group);
+        
+        return a;
+    }
+    
+    void addUnique(List<Integer> lst, int v) {
+        for (int i: lst) {
+            if (i == v)
+                return;
+        }
+        lst.add(v);
+    }
+}
+
+
+=============================================================================
+
+class Solution {    
+    public int largestIsland(int[][] grid) {
+        int group = 2;
+        List<Integer> lst = new ArrayList<>();
+        int max = 0;
+        for (int i = 0; i < grid.length; ++i) {
+            for (int j = 0; j < grid[0].length; ++j) {
+                if (grid[i][j] == 1) {
+                    int ar = area(grid, i, j, group);
+                    if (ar > max)
+                        max = ar;
+                    lst.add(ar);
+                    ++group;
+                }
+            }
+        }
+        
+        if (max == grid.length * grid[0].length)
+            return max;
+        
         for (int i = 0; i < grid.length; ++i) {
             for (int j = 0; j < grid[0].length; ++j) {
                 if (grid[i][j] != 0)
