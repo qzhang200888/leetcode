68366358c88d9335d672bdb1ee23ea55f017d56e
Nz1228 <nancyzhang2017@gmail.com>
2021-12-06 15:37:51 -0800

    add 1631

---
+++ java/1631PathWithMinimumEffort.java
@@ -0,0 +1,46 @@
+class Solution {
+    class Node {
+        public int row;
+        public int col;
+        public int effort;
+        public Node(int row, int col, int effort) {
+            this.row = row;
+            this.col = col;
+            this.effort = effort;
+        }
+    }
+    public int minimumEffortPath(int[][] heights) {
+        int[][] costs = new int[heights.length][heights[0].length];
+        for (int[] row: costs) {
+            Arrays.fill(row, Integer.MAX_VALUE);
+        }
+        
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(new Node(0, 0, 0));
+        costs[0][0] = 0;
+        while (!queue.isEmpty()) {
+            Node n = queue.poll();
+            if (n.row > 0 && costs[n.row - 1][n.col] > Math.max(n.effort, Math.abs(heights[n.row][n.col] - heights[n.row - 1][n.col]))) {
+                queue.add(new Node(n.row - 1, n.col, Math.max(n.effort, Math.abs(heights[n.row][n.col] - heights[n.row - 1][n.col]))));
+                costs[n.row - 1][n.col] = Math.max(n.effort, Math.abs(heights[n.row][n.col] - heights[n.row - 1][n.col]));
+            }
+            
+            if (n.row < heights.length - 1 && costs[n.row + 1][n.col] > Math.max(n.effort, Math.abs(heights[n.row][n.col] - heights[n.row + 1][n.col]))) {
+                queue.add(new Node(n.row + 1, n.col, Math.max(n.effort, Math.abs(heights[n.row][n.col] - heights[n.row + 1][n.col]))));
+                costs[n.row + 1][n.col] = Math.max(n.effort, Math.abs(heights[n.row][n.col] - heights[n.row + 1][n.col]));
+            }
+            
+            if (n.col > 0 && costs[n.row][n.col - 1] > Math.max(n.effort, Math.abs(heights[n.row][n.col] - heights[n.row][n.col - 1]))) {
+                queue.add(new Node(n.row, n.col - 1, Math.max(n.effort, Math.abs(heights[n.row][n.col] - heights[n.row][n.col - 1]))));
+                costs[n.row][n.col - 1] = Math.max(n.effort, Math.abs(heights[n.row][n.col] - heights[n.row][n.col - 1]));
+            }
+            
+            if (n.col < heights[0].length - 1 && costs[n.row][n.col + 1] > Math.max(n.effort, Math.abs(heights[n.row][n.col] - heights[n.row][n.col + 1]))) {
+                queue.add(new Node(n.row, n.col + 1, Math.max(n.effort, Math.abs(heights[n.row][n.col] - heights[n.row][n.col + 1]))));
+                costs[n.row][n.col + 1] = Math.max(n.effort, Math.abs(heights[n.row][n.col] - heights[n.row][n.col + 1]));
+            }
+        }
+        
+        return costs[costs.length - 1][costs[0].length - 1];
+    }
+}
