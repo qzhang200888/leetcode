0bfb2336d77b8879b01891123237cd11a7e4688d
Nz1228 <nancyzhang2017@gmail.com>
2021-12-13 17:45:09 -0800

    update 49

--- java/49GroupAnagrams.java
+++ java/49GroupAnagrams.java
@@ -1,3 +1,27 @@
+
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+        for (String s: strs) {
+            char[] chars = s.toCharArray();
+            Arrays.sort(chars);
+            String key = new String(chars);
+            if (!map.containsKey(key))
+                map.put(key, new ArrayList<>());
+            map.get(key).add(s);
+        }
+        
+        List<List<String>> res = new ArrayList<>();
+        for (String key: map.keySet()) {
+            res.add(map.get(key));
+        }
+        
+        return res;
+    }
+}
+
+================================================================
+
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
         Map<String, List<String>> map = new HashMap<String, List<String>>();
