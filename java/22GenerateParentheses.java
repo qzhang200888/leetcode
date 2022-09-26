58daa073936913fe80c15f671990f2ed712b1c0c
Qing Zhang <qzhang200888@gmail.com>
2022-09-26 11:24:22 -0700

    update 22

--- java/22GenerateParentheses.java
+++ java/22GenerateParentheses.java
@@ -20,3 +20,35 @@ class Solution {
         return res;
     }
 }
+
+=====================================================================================
+
+class Solution {
+    void generateParenthesis(int n, int len, int balance, int leftCount, StringBuilder sb, List<String> res) {
+        if (len == 2 * n) {
+            res.add(sb.toString());
+            return;
+        }
+        
+        if (balance > 0) {
+            sb.append(")");
+            generateParenthesis(n, len + 1, balance - 1, leftCount, sb, res);
+            sb.setLength(sb.length() - 1);
+        }
+        
+        if (leftCount < n) {
+            sb.append("(");
+            generateParenthesis(n, len + 1, balance + 1, leftCount + 1, sb, res);
+            sb.setLength(sb.length() - 1);
+        }
+    }
+
+    public List<String> generateParenthesis(int n) {
+        List<String> res = new ArrayList<>();
+        StringBuilder sb = new StringBuilder();
+        
+        generateParenthesis(n, 0, 0, 0, sb, res);
+        
+        return res;
+    }
+}
